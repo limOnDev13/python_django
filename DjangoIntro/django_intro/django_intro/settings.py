6b59a0df59615102a159044568a03ef2e60b46da
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-09 15:07:59 +0500

    Add shopapp/templates in TEMPLATES

--- DjangoIntro/django_intro/django_intro/settings.py
+++ DjangoIntro/django_intro/django_intro/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.path.join(os.path.dirname(__file__), "..")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'django_intro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_DIR, "shopapp", "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
