9bbdaf0a3416ff4dab4dbf0ab974c1e4f2c6eacd
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-09 21:19:37 +0500

    Add view-func shop_index; add view-func products_list

--- DatabaseAndModels/mysite/shopapp/views.py
+++ DatabaseAndModels/mysite/shopapp/views.py
@@ -1,3 +1,27 @@
+from typing import Dict, Any
+
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+from shopapp.models import Product
+
 
 # Create your views here.
+def shop_index(request: HttpRequest) -> HttpResponse:
+    context: Dict[str, Any] = {
+        "links": [
+            ("/products", "http://localhost:8000/shop/products/"),
+            ("/orders", "http://localhost:8000/shop/orders/"),
+        ]
+    }
+    return render(
+        request, "shopapp/shop-index.html", context=context
+    )
+
+
+def products_list(request: HttpRequest) -> HttpResponse:
+    context: Dict[str, Any] = {
+        "products": Product.objects.all()
+    }
+    return render(request, "shopapp/products-list.html", context=context)
+
