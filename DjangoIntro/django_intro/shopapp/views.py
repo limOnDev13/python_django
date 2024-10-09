f76d3608bd89581dcac3311045fa6516c2619445
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-09 15:08:58 +0500

    Add view-func shop_index

--- DjangoIntro/django_intro/shopapp/views.py
+++ DjangoIntro/django_intro/shopapp/views.py
@@ -1,3 +1,16 @@
+from typing import Dict, Any
+
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def shop_index(request: HttpRequest) -> HttpResponse:
+    context: Dict[str, Any] = {
+        "value1": 2,
+        "value2": 3,
+        "headers": [
+            "a" * num for num in range(5)
+        ]
+    }
+    return render(request, "shopapp/shop-index.html", context=context)
