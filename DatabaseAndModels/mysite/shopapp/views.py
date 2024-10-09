9e6418513de8f6d82570a288b68133fca3fa55f8
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-09 21:36:29 +0500

    Add view-func orders_list

--- DatabaseAndModels/mysite/shopapp/views.py
+++ DatabaseAndModels/mysite/shopapp/views.py
@@ -3,7 +3,7 @@ from typing import Dict, Any
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-from shopapp.models import Product
+from shopapp.models import Product, Order
 
 
 # Create your views here.
@@ -25,3 +25,9 @@ def products_list(request: HttpRequest) -> HttpResponse:
     }
     return render(request, "shopapp/products-list.html", context=context)
 
+
+def orders_list(request: HttpRequest) -> HttpResponse:
+    context: Dict[str, Any] = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all()
+    }
+    return render(request, "shopapp/orders-list.html", context=context)
