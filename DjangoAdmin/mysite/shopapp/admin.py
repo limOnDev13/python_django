5185e792e139457cfa6ff78221cfdf6cd9a196e0
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-10 12:02:48 +0500

    Display the model fields on the page in the admin panel

--- DjangoAdmin/mysite/shopapp/admin.py
+++ DjangoAdmin/mysite/shopapp/admin.py
@@ -6,9 +6,9 @@ from .models import Product, Order
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = "pk", "name", "description", "price", "archived"
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = "pk", "description", "address", "created_at", "user"
