6933789d4927fd65172d560170f5c3b1385f4f67
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-10 20:46:20 +0500

    Add view-func create_product

--- Forms/mysite/shopapp/views.py
+++ Forms/mysite/shopapp/views.py
@@ -2,9 +2,10 @@ from timeit import default_timer
 
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from .models import Product, Order
+from .forms import ProductForm
 
 
 def shop_index(request: HttpRequest):
@@ -39,3 +40,21 @@ def orders_list(request: HttpRequest):
         "orders": Order.objects.select_related("user").prefetch_related("products").all(),
     }
     return render(request, 'shopapp/orders-list.html', context=context)
+
+
+def create_product(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+        url = reverse("shopapp:products_list")
+        return redirect(url)
+
+    else:
+        form = ProductForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, "shopapp/create-product.html", context=context)
+
