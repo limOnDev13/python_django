36b780df052b35738d9357c3a339407ed168ab41
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-10 12:12:46 +0500

    Add verbose_name to every field; Add classes Meta

--- DjangoAdmin/mysite/shopapp/models.py
+++ DjangoAdmin/mysite/shopapp/models.py
@@ -4,17 +4,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=30, null=False)
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    discount = models.PositiveSmallIntegerField(default=0)
+    name = models.CharField(max_length=30, null=False, verbose_name="Product name")
+    description = models.TextField(blank=True, null=True, verbose_name="Product description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Price without discount")
+    discount = models.PositiveSmallIntegerField(default=0, verbose_name="Current discount")
     archived = models.BooleanField(default=True, null=False)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "products"
 
 
 class Order(models.Model):
-    description = models.TextField(null=True)
-    address = models.CharField(max_length=200, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    products = models.ManyToManyField(Product, related_name="orders")
+    description = models.TextField(null=True, verbose_name="Order description")
+    address = models.CharField(max_length=200, null=False, verbose_name="User address")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Date and time of creation order")
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="User name")
+    products = models.ManyToManyField(Product, related_name="orders", verbose_name="List of products in order")
 
+    class Meta:
+        ordering = ["-created_at", "user", "pk"]
+        verbose_name_plural = "orders"
