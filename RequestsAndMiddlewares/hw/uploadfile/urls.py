a46156c3374201752c17bce02b375524bee242e9
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-10 18:03:05 +0500

    Register url /upload/throttling/

--- RequestsAndMiddlewares/hw/uploadfile/urls.py
+++ RequestsAndMiddlewares/hw/uploadfile/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import upload_file
+from .views import upload_file, throttling
 
 app_name = "shopapp"
 
 urlpatterns = [
-    path("", upload_file, name="index"),
+    path("", upload_file, name="upload"),
+    path("throttling/", throttling, name="throttling"),
 ]
