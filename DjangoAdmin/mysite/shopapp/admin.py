ffc9b9ef3dca9557e6c9483d0652017afda49727
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-10 12:25:34 +0500

    Display the "to many" relationship on the model page, where the relationship is not displayed by default (in this case, it is a Product to which you need to add an Order to the model)

--- DjangoAdmin/mysite/shopapp/admin.py
+++ DjangoAdmin/mysite/shopapp/admin.py
@@ -3,11 +3,21 @@ from django.contrib import admin
 from .models import Product, Order
 
 
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description", "price", "archived"
     search_fields = "pk", "name", "description"
+    inlines = [
+        OrderInline,
+    ]
+
+    def get_queryset(self, request):
+        return Product.objects.prefetch_related("orders")
 
 
 @admin.register(Order)
