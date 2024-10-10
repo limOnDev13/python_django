96af447a8fec412c4a57e7d4d138efb8cad10032
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-10 12:17:28 +0500

    Add a search for two fields in the models

--- DjangoAdmin/mysite/shopapp/admin.py
+++ DjangoAdmin/mysite/shopapp/admin.py
@@ -7,8 +7,10 @@ from .models import Product, Order
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description", "price", "archived"
+    search_fields = "pk", "name", "description"
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = "pk", "description", "address", "created_at", "user"
+    search_fields = "pk", "description", "user"
