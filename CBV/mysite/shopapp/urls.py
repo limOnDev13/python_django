4f632d1f58ef5980a62d20a18cc02e381ed6e05f
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-13 13:48:22 +0500

    Register path /shop/products/update/<int:pk>

--- CBV/mysite/shopapp/urls.py
+++ CBV/mysite/shopapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from .views import shop_index, groups_list, ProductsListView, orders_list, ProductDetailView
+from .views import (shop_index, groups_list,
+                    ProductsListView, orders_list, ProductDetailView,
+                    ProductUpdateView)
 
 app_name = "shopapp"
 
@@ -9,5 +11,6 @@ urlpatterns = [
     path("groups/", groups_list, name="groups_list"),
     path("products/", ProductsListView.as_view(), name="products_list"),
     path("products/<int:pk>", ProductDetailView.as_view(), name="product_details"),
+    path("products/update/<int:pk>", ProductUpdateView.as_view(), name="product_update"),
     path("orders/", orders_list, name="orders_list"),
 ]
