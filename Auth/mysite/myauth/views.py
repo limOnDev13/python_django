074bc6e8ef24c6705a3cc24a5172b560efa908ec
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-14 12:12:53 +0500

    Add view class MyLogoutView

--- Auth/mysite/myauth/views.py
+++ Auth/mysite/myauth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class MyLogoutView(LogoutView):
+    next_page = reverse_lazy("myauth:login")
+
+
+def main_page(request: HttpRequest) -> HttpResponse:
+    return render(request, "myauth/index.html")
