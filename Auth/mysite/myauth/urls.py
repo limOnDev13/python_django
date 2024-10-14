ee4b6dc8f9256deca61fc54deb2f3412dad845ee
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-14 12:12:34 +0500

    Register pathes /, /login/ and /logout/

--- Auth/mysite/myauth/urls.py
+++ Auth/mysite/myauth/urls.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import MyLogoutView, main_page
+
+app_name = "myauth"
 
 urlpatterns = [
-    # path('', admin.site.urls),
+    path("", main_page, name="main_page"),
+    path(
+        "login/", LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        MyLogoutView.as_view(),
+        name="logout"
+    ),
 ]
