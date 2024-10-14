7a10626570e4cd74d33967c6903e4b39721534c5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-14 12:26:15 +0500

    Register paths set_session/ and get_session/

--- Auth/mysite/myauth/urls.py
+++ Auth/mysite/myauth/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import MyLogoutView, main_page, set_cookie_view, get_cookie_view
+from .views import (MyLogoutView, main_page, set_cookie_view,
+                    get_cookie_view, get_session_view, set_session_view)
 
 app_name = "myauth"
 
@@ -22,4 +23,6 @@ urlpatterns = [
     ),
     path("set_cookie/", set_cookie_view, name="set_cookie"),
     path("get_cookie/", get_cookie_view, name="get_cookie"),
+    path("set_session/", set_session_view, name="set_session"),
+    path("get_session/", get_session_view, name="get_session"),
 ]
