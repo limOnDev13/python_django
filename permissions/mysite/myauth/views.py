a69ad9305b4f3e7df47ee9294046b72d8cb7f01a
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-15 12:49:29 +0500

    Add view-func main_page_view

--- permissions/mysite/myauth/views.py
+++ permissions/mysite/myauth/views.py
@@ -1,4 +1,11 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.http import HttpRequest, HttpResponse
+
+
+def main_page_view(request: HttpRequest) -> HttpResponse:
+    return render(request, "myauth/main-page.html")
 
 
 class AboutMeView(TemplateView):
