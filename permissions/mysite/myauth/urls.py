09a1d36853ecae2c0188f9815e554f3f694db6cb
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-15 12:48:21 +0500

    Register url patterns /accounts/main-page and /accounts/logout

--- permissions/mysite/myauth/urls.py
+++ permissions/mysite/myauth/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import main_page_view
 
 
 from .views import (
@@ -8,5 +11,19 @@ from .views import (
 app_name = "myauth"
 
 urlpatterns = [
+    path("main-page/", main_page_view, name="main_page"),
+    path(
+        "login/",
+        LoginView.as_view(
+            template_name="myauth/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login"
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(),
+        name="logout"
+    ),
     path("about-me/", AboutMeView.as_view(), name="about-me"),
 ]
