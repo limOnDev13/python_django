d22ce43eab659410080d4a4cf13d9dc3072061eb
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-15 14:57:23 +0500

    Add view-class RegisterView

--- permissions/mysite/myauth/views.py
+++ permissions/mysite/myauth/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+
+from .models import Profile
 
 
 def main_page_view(request: HttpRequest) -> HttpResponse:
@@ -10,3 +15,22 @@ def main_page_view(request: HttpRequest) -> HttpResponse:
 
 class AboutMeView(TemplateView):
     template_name = "myauth/about-me.html"
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "myauth/register.html"
+    success_url = reverse_lazy("myauth:about-me")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password,
+        )
+        login(request=self.request, user=user)
+        return response
