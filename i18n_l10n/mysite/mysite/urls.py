5d22846efe495f23a7c1fbe28b15f3fa839e07d1
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-20 20:45:03 +0500

    Make all applications internalized

--- i18n_l10n/mysite/mysite/urls.py
+++ i18n_l10n/mysite/mysite/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/', include('shopapp.urls')),
-    path('accounts/', include('myauth.urls')),
-]
+urlpatterns = list()
+
+urlpatterns.extend(
+    i18n_patterns(
+        path('admin/', admin.site.urls),
+        path('shop/', include('shopapp.urls')),
+        path('accounts/', include('myauth.urls')),
+    )
+)
 
 if settings.DEBUG:
     urlpatterns.extend(
