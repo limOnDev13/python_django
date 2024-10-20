71ad470dcbffc18390d99ab39086dc905749a38e
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-20 11:25:51 +0500

    Copy project from previous module

--- Files/mysite/myauth/urls.py
+++ Files/mysite/myauth/urls.py
@@ -1,40 +1,35 @@
-from django.contrib.auth.views import LoginView
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import main_page_view, RegisterView, ProfileUpdateView
 
 
 from .views import (
-    get_cookie_view,
-    set_cookie_view,
-    set_session_view,
-    get_session_view,
-    MyLogoutView,
     AboutMeView,
-    RegisterView,
-    FooBarView,
 )
 
 app_name = "myauth"
 
 urlpatterns = [
-    # path("login/", login_view, name="login"),
+    path("main-page/", main_page_view, name="main_page"),
     path(
         "login/",
         LoginView.as_view(
             template_name="myauth/login.html",
             redirect_authenticated_user=True,
         ),
-        name="login",
+        name="login"
     ),
-    # path("logout/", logout_view, name="logout"),
-    path("logout/", MyLogoutView.as_view(), name="logout"),
+    path(
+        "logout/",
+        LogoutView.as_view(),
+        name="logout"
+    ),
+    path(
+        "register/",
+        RegisterView.as_view(),
+        name="register",
+    ),
     path("about-me/", AboutMeView.as_view(), name="about-me"),
-    path("register/", RegisterView.as_view(), name="register"),
-
-    path("cookie/get/", get_cookie_view, name="cookie-get"),
-    path("cookie/set/", set_cookie_view, name="cookie-set"),
-
-    path("session/set/", set_session_view, name="session-set"),
-    path("session/get/", get_session_view, name="session-get"),
-
-    path("foo-bar/", FooBarView.as_view(), name="foo-bar"),
+    path("update-profile/", ProfileUpdateView.as_view(), name="update-profile"),
 ]
