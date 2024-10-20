fd08cc33b94aaee83e598cf2e9b0f2e68c33318b
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-20 13:44:39 +0500

    Add view-classes UserListView, UserDetailView, AvatarUpdateView; add field "avatar" in form in ProfileUpdateView

--- Files/mysite/myauth/views.py
+++ Files/mysite/myauth/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, TemplateView, ListView, DetailView
 from django.http import HttpRequest, HttpResponse
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from .models import Profile
 
@@ -39,9 +39,35 @@ class RegisterView(CreateView):
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
-    fields = "biography", "agreement_accepted"
+    fields = "biography", "agreement_accepted", "avatar"
     template_name = "myauth/profile_update.html"
     success_url = reverse_lazy("myauth:about-me")
 
     def get_object(self, queryset=None):
         return self.request.user.profile
+
+
+class UserListView(ListView):
+    queryset = User.objects.select_related("profile")
+    template_name = "myauth/user_list.html"
+
+
+class UserDetailView(DetailView):
+    queryset = User.objects.select_related("profile")
+    template_name = "myauth/about-user.html"
+    context_object_name = "object"
+
+
+class AvatarUpdateView(UserPassesTestMixin, UpdateView):
+    model = Profile
+    fields = "avatar",
+    template_name = "myauth/avatar_update.html"
+
+    def get_success_url(self):
+        return reverse(
+            "myauth:about-user",
+            kwargs={"pk": self.object.pk},
+        )
+
+    def test_func(self):
+        return self.request.user.is_staff or self.get_object().pk == self.request.user.pk
