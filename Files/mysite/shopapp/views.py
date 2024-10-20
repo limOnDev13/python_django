52a8b981efac183c6ae5f87209c8675a5d18319a
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-20 13:44:54 +0500

    Minor fix

--- Files/mysite/shopapp/views.py
+++ Files/mysite/shopapp/views.py
@@ -8,7 +8,6 @@ from django.shortcuts import render, reverse
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.models import Permission
 
 from .models import Product, Order
 
