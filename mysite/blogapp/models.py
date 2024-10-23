71862960aeb7b20c11f4718d77a0c33cf9242ff9
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-23 19:12:56 +0500

    Fix model Tag

--- mysite/blogapp/models.py
+++ mysite/blogapp/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     name = models.CharField(max_length=40)
 
 
-class Tag(models.CharField):
+class Tag(models.Model):
     name = models.CharField(max_length=20)
 
 
