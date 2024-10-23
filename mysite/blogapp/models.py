928d8aadd01586d6ab2f086b3b50df0096fdc08c
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-23 19:11:28 +0500

    Create models Author, Category, Tag and Article

--- mysite/blogapp/models.py
+++ mysite/blogapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Tag(models.CharField):
+    name = models.CharField(max_length=20)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
