00cacd0f140d9930ca0e71b5430313724995c728
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-23 16:44:39 +0500

    Add app drf_spectacular

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "django_filters",
+    "drf_spectacular",
 
     'shopapp.apps.ShopappConfig',
     'myauth.apps.MyauthConfig',
@@ -156,5 +157,13 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend"
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema"
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Mysite Project API",
+    "DESCRIPTION": "Mysite with shop app and custom auth",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
