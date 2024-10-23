9a1aafd24718e1df23f5b14494a7964b97564062
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-10-23 20:57:08 +0500

    Fix models - Author.name, Category.name, Tag.name and Article.name now is not nullable and cannot be blank; Article.content now is nullable and can be blank

--- mysite/blogapp/models.py
+++ mysite/blogapp/models.py
@@ -3,21 +3,21 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False, blank=False)
     bio = models.TextField()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, null=False, blank=False)
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, null=False, blank=False)
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    title = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField(null=False, blank=True, default='')
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
